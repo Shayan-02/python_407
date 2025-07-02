9b9108871c96243b57a9ece593dca583dc9b032f
Shayan Shahbazi <upm200@gmail.com>
2025-07-02 15:29:10 +0330

    j02

---
+++ j02/data_types.py
@@ -0,0 +1,14 @@
+# int:
+a = 1
+
+# float:
+b = 1.1
+
+# string:
+c = "hi"
+
+# boolean
+d = True
+e = False
+n = not e
+# --------------------
---
+++ j02/tamrin.py
@@ -0,0 +1,6 @@
+fname = "ali"
+lname = "rezaee"
+a = 10
+b = 10.0
+print(a == b)
+
