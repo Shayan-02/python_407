3393f52510e56c747d434e7aab7b451c63d4e10b
Shayan Shahbazi <upm200@gmail.com>
2026-07-09 12:16:27 +0330

    Create 1.py

---
+++ tamrin/tamrin1/1.py
@@ -0,0 +1,24 @@
+# int
+a = 10
+
+# float
+b = 7.43
+
+# str
+c = "alireza"
+
+# bool
+d = True
+e = False
+
+# output
+print("value:", a, "\ntype:", type(a))
+print("----------------")
+print("value:", b, "\ntype:", type(b))
+print("----------------")
+print("value:", c, "\ntype:", type(c))
+print("----------------")
+print("value:", d, "\ntype:", type(d))
+print("----------------")
+print("value:", e, "\ntype:", type(e))
+print("----------------")
