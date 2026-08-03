b0960f8654e69f844c4ffa51b83f5aad357a8a5b
Shayan Shahbazi <upm200@gmail.com>
2026-08-03 10:00:31 +0330

    j09

--- j09/5.py
+++ j09/5.py
@@ -1,6 +1,16 @@
-lst = ["ali", "reza", "mohammad"]
+lst = ["ali", "reza", "mohammad", "reza"]
 
 lst.append("amir")
 lst.insert(1, "ahmad")
 
+lst.pop()
+lst.pop(1)
+lst.remove("reza")
+
+print(lst)
+
+lst.clear()
+print(lst)
+
+del lst
 print(lst)
